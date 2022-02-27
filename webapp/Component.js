ca0b289f35e86aff5cf3a9ff5f5d67ed5699da2e
abhisekch59 <abhisekch59@gmail.com>
2022-02-27 21:02:20 +0530

    Initial Setup

---
+++ webapp/Component.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/ComponentSupport"
+], function (UIComponent) {
+    "use strict";
+    return UIComponent.extend("com.demo.d3.Component", {
+        metadata: {
+            manifest: "json"
+        },
+    });
+});
